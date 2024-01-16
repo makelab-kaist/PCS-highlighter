7cc3205e1ed000c24b0b2dfecb7f03dddbcddf20
Andrea <andrea@kaist.ac.kr>
2024-01-16 21:12:47 +1100

    Refactoring

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import './style.css';
 import tutorial from './tutorial.gif';
 
+const code = `
+  setInterval(()=>{
+    const papers = document.querySelectorAll('li.acPaper');
+    papers.forEach(paper => {
+      paper.style.backgroundColor = 'yellow';
+    });
+  }, 500)
+`;
+
 document.querySelector('#app').innerHTML = `
   <div>
     <h2>Drag me: </h2>
     <button type="button" class="btn btn-primary">
       <a href="
-      javascript: (() => {
-        setInterval(()=>{
-                const papers = document.querySelectorAll('li.acPaper');
-                papers.forEach(paper => {
-                  paper.style.backgroundColor = 'yellow';
-                });
-        }, 500)})();
-        ">
+      javascript: (() => {${code}})()">
         PCS highlighter
       </a>
     </button>
